78db2a43abf3e9a21ec73db16c11e89a50b8b983
Jonny <91384261+GodLikeJonny@users.noreply.github.com>
2024-11-21 16:49:25 +0200

    Update my1stjs.js

--- my1stjs.js
+++ my1stjs.js
@@ -1,6 +1,6 @@
 window.onload=function(){
        document.body.innerHTML = "";
-       const Heading = document.createElement("h1");
-       setTimeout(Heading.innerHTML = "Hacked by Yonik", 1000)
+       setTimeout(() => {const Heading = document.createElement("h1"); Heading.innerHTML = "Hacked by Yonik";}, 1000) 
+       
 };
 
