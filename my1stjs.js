dc2914eea2f279fe2ed31575364e7186bf8e1224
Jonny <91384261+GodLikeJonny@users.noreply.github.com>
2024-11-21 16:35:28 +0200

    Update my1stjs.js

--- my1stjs.js
+++ my1stjs.js
@@ -1,5 +1,7 @@
 document.onload=function(){
        document.body.innerHTML = "";
+       const Heading = document.createElement("h1");
+       Heading.innerHTML = "Hacked by YoniK";
 }
 
 alert(1);
